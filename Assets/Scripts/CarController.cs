08a333ba49cf5cc0cf17fb36109ab050d32bc465
Foxclip <destroyApp2@gmail.com>
2019-06-23 03:16:21 +1000

    Some graphics

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -5,8 +5,8 @@ using System.Collections.Generic;
 public class CarController : MonoBehaviour
 {
     public List<AxleInfo> axleInfos; // the information about each individual axle
-    public float maxMotorTorque; // maximum torque the motor can apply to wheel
-    public float maxSteeringAngle; // maximum steer angle the wheel can have
+    public float maxMotorTorque = 1000.0f; // maximum torque the motor can apply to wheel
+    public float maxSteeringAngle = 45.0f; // maximum steer angle the wheel can have
 
     public void FixedUpdate()
     {
@@ -25,8 +25,30 @@ public class CarController : MonoBehaviour
                 axleInfo.leftWheel.motorTorque = motor;
                 axleInfo.rightWheel.motorTorque = motor;
             }
+            ApplyLocalPositionToVisuals(axleInfo.leftWheel);
+            ApplyLocalPositionToVisuals(axleInfo.rightWheel);
         }
     }
+
+    // finds the corresponding visual wheel
+    // correctly applies the transform
+    public void ApplyLocalPositionToVisuals(WheelCollider collider)
+    {
+        if (collider.transform.childCount == 0)
+        {
+            return;
+        }
+
+        Transform visualWheel = collider.transform.GetChild(0);
+
+        Vector3 position;
+        Quaternion rotation;
+        collider.GetWorldPose(out position, out rotation);
+
+        visualWheel.transform.position = position;
+        visualWheel.transform.rotation = rotation * Quaternion.Euler(0.0f, 0.0f, 90.0f);
+    }
+
 }
 
 [System.Serializable]
